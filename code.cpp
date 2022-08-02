3b941faf66e98bb0aa0fa73e909fc753184a9c3d
treyder38 <95418433+treyder38@users.noreply.github.com>
2022-08-02 12:40:59 +0400

    Create code.cpp

---
+++ code.cpp
@@ -0,0 +1,31 @@
+#define _CRT_SECURE_NO_WARNINGS
+#include <iostream>
+#include <vector>
+#include <map>
+#include <set>
+#include <queue>
+#include <cmath>
+#include <numeric>
+#include <algorithm>
+#include <functional>
+#define sqr(i) ((i) * (i))
+#define all(i) (i).begin(), (i).end()
+#define rep(i) for (int rep_iter = 0; rep_iter < i; ++rep_iter)
+using namespace std;
+typedef long long ll;
+typedef pair <int, int> pii;
+const int INF = 1e9 + 7;
+const long long LINF = 1LL << 62LL;
+
+void solve() {
+
+}
+
+int main() {
+#ifndef ONLINE_JUDGE
+	freopen("input.txt", "r", stdin); freopen("output.txt", "w", stdout);
+#endif
+	ios_base::sync_with_stdio(0); cin.tie(0);
+
+	solve();
+}
